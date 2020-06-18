f21a8465cfbd955738c0031e8d73c62e943280a5
William Stella <62618488+wastella@users.noreply.github.com>
2020-06-18 10:53:09 -0400

    Created a Lexer (#4)
    A lexer to tokenize the following chars:
    
    Integers
    Decimal points (the decimal point will decide whether it is a float or an integer)
    Basic Operators ["+", "-", "*", "/"]. Can differentiate between negative sign and subtraction sign
    Spaces (" ")
    Parenthesis [")", "("]

--- calculator/lexer_test.py
+++ calculator/lexer_test.py
@@ -5,20 +5,28 @@ from lexer import lex
 
 
 def test_basecase():
-    # assert lex('1 + 1') == [ 1, '+', 1 ]
-    pass
+    assert lex("1 + 1") == [1, "+", 1]
 
 
 def test_negative_weirdness():
-    # assert lex('1 - -1') == [ 1, '-', -1 ]
-    pass
+    assert lex("1 - -1") == [1, "-", -1]
 
 
 def test_no_space():
-    # assert lex('-1.2*2') == [ -1.2, '*', 2 ]
-    pass
+    assert lex("-1.2*2") == [-1.2, "*", 2]
+
+
+def test_multiple_spaces():
+    assert lex("1  + 1") == [1, "+", 1]
 
 
 def test_parens():
-    # assert lex('(2 + 3) - 3.2') == [ '(', 2, '+', 3, ')', '-', 3.2 ]
-    pass
+    assert lex("(2 + 3) - 3.2") == ["(", 2, "+", 3, ")", "-", 3.2]
+
+
+def test_RPN():
+    assert lex("1 1 +") == [1, 1, "+"]
+
+
+def test_RPN_with_spaces():
+    assert lex("1  1 +") == [1, 1, "+"]
