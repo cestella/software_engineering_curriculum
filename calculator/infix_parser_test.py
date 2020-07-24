f0203274e704b7b9f77a4b7b655c6f4b9560383b
William Stella <62618488+wastella@users.noreply.github.com>
2020-07-24 15:15:02 -0400

    Infix to RPN Converter (#9)
    * Small changes to the rpn parser pull request
    
    * Woking on the shunting yard algorithm
    
    * More converter work
    
    * Not much today
    
    * Working on infix_converter
    
    * Debugged
    
    * ran black
    
    * Finished debugging my infix_converter
    
    * ran black
    
    * made docstring and some small changes
    
    * Decoupled peek, pop, enqueue, dequeue and push
    
    * Finished changes requested in the comments
    
    * Final changes (I think)
    
    * minor nit, last changes
    
    * updated README.md

---
+++ calculator/infix_parser_test.py
@@ -0,0 +1,81 @@
+from parser import rpn
+from lexer import lex
+from parser import infix
+
+
+def test_basic():
+    assert rpn(infix(lex("1 + 1"))) == (1 + 1)
+
+
+def test_multi_level():
+    assert rpn(infix(lex("(3 + 6) * 7"))) == (3 + 6) * 7
+
+
+def test_subtraction():
+    assert rpn(infix(lex("6 - 3"))) == 6 - 3
+
+
+def test_division():
+    assert rpn(infix(lex("9 / 3"))) == 9 / 3
+
+
+def test_negative_answer():
+    assert rpn(infix(lex("5 - 8"))) == 5 - 8
+
+
+def test_negative_number():
+    assert rpn(infix(lex("-5 + 8"))) == -5 + 8
+
+
+def test_float_answer():
+    assert rpn(infix(lex("(5 + 8) / 7"))) == (5 + 8) / 7
+
+
+def test_fraction():
+    assert rpn(infix(lex("(1 + 5) / 7"))) == (1 + 5) / 7
+
+
+def test_float_number():
+    assert rpn(infix(lex("(7 - 3) / 7.0"))) == (7 - 3) / 7.0
+
+
+def test_negative_division():
+    assert rpn(infix(lex("(-5 + 3) / 2"))) == (-5 + 3) / 2
+
+
+def test_negative_and_float_division():
+    assert rpn(infix(lex("(-5 + 3.0) / 2"))) == (-5 + 3.0) / 2
+
+
+def test_non_math_operator():
+    try:
+        rpn(infix(lex("3 4 op")))
+    except ValueError:
+        pass
+    else:
+        raise ValueError("Expected exception, but it didn't raise")
+
+
+def test_no_operator():
+    try:
+        rpn(infix(lex("3 5")))
+    except ValueError:
+        pass
+    else:
+        raise ValueError("Expected exception, bit it didnt raise")
+
+
+def test_num():
+    assert rpn(infix(lex("1"))) == 1
+
+
+def test_decimal():
+    assert rpn(infix(lex("5.0"))) == 5.0
+
+
+def test_negative():
+    assert rpn(infix(lex("-5"))) == -5
+
+
+def test_negative_decimal():
+    assert rpn(infix(lex("-5.0")))
--- calculator/parser.py
+++ calculator/parser.py
@@ -1,4 +1,15 @@
+import lexer
+import numbers
+
 BINARY_OPERATORS = ["+", "-", "*", "/"]
+
+OPS_RANKING = {"(": 1, ")": 1, "/": 2, "*": 3, "+": 4, "-": 5}
+
+OPS_ASSOSIATIVITY = {"+": "l", "-": "l", "*": "l", "/": "l"}
+
+
+def is_function(string):
+    return False
 
 
 def binary_operator(lhs, rhs, op):
@@ -40,34 +51,6 @@ def binary_operator(lhs, rhs, op):
         )
 
 
-def push(e, stack):
-    """
-    Pushes the element (e) to the given stack, which mutates the stack
-
-    Parameters
-    ----------
-    e : element
-        The element to be pushed on top of the stack
-
-    stack : list
-        The stack that e is going to be pushed onto
-
-    """
-    stack.append(e)
-
-
-def pop(stack):
-    """
-    Retrieves the last element from the given stack and deletes it, which mutates it
-
-    Parameters
-    ----------
-    stack : list
-        The stack (in list form) to be operated on
-    """
-    return stack.pop()
-
-
 def is_numeric_token(token):
     """
     Says whether or not the token input is a number
@@ -82,7 +65,65 @@ def is_numeric_token(token):
     bool: Whether or not this token is a numeric character
     """
 
-    return type(token) == float or type(token) == int
+    return isinstance(token, numbers.Number)
+
+
+def push(value, stack):
+    """
+    Pushes the element (value) to the given stack, which mutates the stack
+    Parameters
+    ----------
+    value : element
+        The element to be pushed on top of the stack
+    stack : list
+        The stack that e is going to be pushed onto
+    """
+    stack.append(value)
+
+
+def pop(stack):
+    """
+    Retrieves the last element from the given stack and deletes it, which mutates iti
+    Parameters
+    ----------
+    stack : list
+        The stack (in list form) to be operated on
+    Returns
+    -------
+    value : char
+        The value at the end of the given stack
+    """
+    return stack.pop()
+
+
+def peek(stack):
+    """
+    Returns the top value of the given stack
+    Parameter
+    ---------
+    stack : list
+        The stack (in list form) to operated on
+    Returns
+    -------
+    token : The top token of the stack
+    """
+    try:
+        return stack[-1]
+    except:
+        pass
+
+
+def enqueue(value, queue):
+    """
+    Pushes the element (value) to the given queue, which mutates the queue
+    Parameters
+    ----------
+    queue : list
+        The queue in list form
+    value : element
+        The item to enqueue to the given queue
+    """
+    queue.append(value)
 
 
 def rpn(tokens):
@@ -139,3 +180,89 @@ def rpn(tokens):
                 ",".join(str(t) for t in tokens)
             )
         )
+
+
+def infix(token_list):
+
+    """
+    Parses Infix notation and returns the equivilant RPN form (Pseudocode used from: https://en.wikipedia.org/wiki/Shunting-yard_algorithm)
+
+    Parameters
+    ==========
+    token_list : list
+        The list of infix tokens
+
+    Returns 
+    --------
+    output : list
+        This is the output queue where the final answer is arranged
+    
+    """
+
+    # Output Queue
+    output = []
+
+    # Operators Stack
+    operators = []
+
+    processed_tokens = 0
+
+    # Iterating through each token in the token_list
+    for token in token_list:
+        if is_numeric_token(token):
+
+            enqueue(token, output)
+            processed_tokens += 1
+
+        elif is_function(token):
+            push(token, operators)
+            processed_tokens += 1
+
+        elif token in lexer.OPS:
+            # If there are tokens to be read then
+            # 1. If the operators stack is not empty (for the first operator, we want to skip this loop and go push the token to the opeators stack)
+            # 2. And the operator on teh operator stack has greater precedence (for order of operations)
+            # 3. Or they have equal precedense and the token is left associative because if the tokens are 1+1+1, then we need to know which way to group them
+            # 4. If the operator on the operator stack is not a "("
+
+            while (
+                (peek(operators) != None)
+                and (
+                    (OPS_RANKING[peek(operators)] > OPS_RANKING[token])
+                    or (
+                        OPS_RANKING[peek(operators)] == OPS_RANKING[token]
+                        and OPS_ASSOSIATIVITY[token] == "l"
+                    )
+                )
+                and (peek(operators) != "(")
+            ):
+                # Pushing the token on top of the operators stack onto the output queue
+                enqueue(pop(operators), output)
+                processed_tokens += 1
+
+            push(token, operators)
+
+        # This is basically useless, that is why we skip it in the operator while loop, but we still need to recognize it as an operator processed
+        if token == "(":
+            push(token, operators)
+            processed_tokens += 1
+
+        elif token == ")":
+            # If the operator on the top of the operator stack is not a "(", then it is an operator, and since this is the end of the group then we have to update the output with the operator
+            # If the top of the operator stack is not a "(" (Left Parenthesis)
+            if peek(operators) != "(" and len(operators) != 0:
+                enqueue(pop(operators), output)
+                processed_tokens += 1
+
+            # Ignoring this char again, but we still need to count it as a operator operated on
+            # If the top of the operator stack is a "(" (Left Parenthesis)
+            if peek(operators) == "(" and len(operators) != 0:
+                pop(operators)
+                processed_tokens += 1
+
+    if len(token_list) - 1 == processed_tokens:
+        while len(operators) != 0:
+            if peek(operators) != "(":
+                enqueue(pop(operators), output)
+
+    return output
