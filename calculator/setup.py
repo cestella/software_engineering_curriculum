f4a8a32ef56ede992bf9b795efb7c0823e4d2334
wastella <william.a.stella@gmail.com>
2020-07-28 09:50:18 -0400

    Ran black again on setup.py

--- calculator/setup.py
+++ calculator/setup.py
@@ -1,32 +1,28 @@
-from setuptools import setup, find_packages 
-  
-with open('requirements.txt') as f: 
-    requirements = f.readlines() 
-  
-long_description = 'A simple command line utility calculator.' 
-  
-setup( 
-        name ='pythoncalculator', 
-        version ='1.0.0', 
-        author ='William Stella', 
-        author_email ='william.a.stella@gmail.com', 
-        url ='https://github.com/cestella/software_engineering_curriculum/tree/master/calculator', 
-        description ='Command line calculator using python', 
-        long_description = long_description, 
-        long_description_content_type ="text/markdown", 
-        license ='APACHE', 
-        packages = find_packages(), 
-        entry_points ={ 
-            'console_scripts': [ 
-                'calc = src.cli:main'
-            ] 
-        }, 
-        classifiers =( 
-            "Programming Language :: Python :: 3", 
-            "License :: OSI Approved :: APACHE License", 
-            "Operating System :: OS Independent", 
-        ), 
-        keywords ='calculator python package williamstella', 
-        install_requires = requirements, 
-        zip_safe = False
-) 
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+
+long_description = "A simple command line utility calculator."
+
+setup(
+    name="pythoncalculator",
+    version="1.0.0",
+    author="William Stella",
+    author_email="william.a.stella@gmail.com",
+    url="https://github.com/cestella/software_engineering_curriculum/tree/master/calculator",
+    description="Command line calculator using python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="APACHE",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["calc = src.cli:main"]},
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: APACHE License",
+        "Operating System :: OS Independent",
+    ),
+    keywords="calculator python package williamstella",
+    install_requires=requirements,
+    zip_safe=False,
+)
