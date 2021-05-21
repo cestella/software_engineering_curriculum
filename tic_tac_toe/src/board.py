63408b54b224783f42fe51f75bb385cc39878955
wastella <william.a.stella@gmail.com>
2021-05-20 21:04:58 -0400

    updated edge cases

--- tic_tac_toe/src/board.py
+++ tic_tac_toe/src/board.py
@@ -20,25 +20,37 @@ class Board:
         self.winner = None
 
     def _convert(self, x, y):
-        return ((x*int(self.dim))+y)-1
+        # Convert the 2 dimensional coordinates into 1d coords that we can use to subset the list of values
+        if x < 0 or y < 0:
+            return False
+
+        elif x > 3 or y > 3:
+            return False
+    
+        else:
+            return ((x*int(self.dim))+y)-1
+
 
     def render_board(self):
+        # Print out the board, in a way humans can understand
         return self.template.format(*self.values)
 
     def raw_board(self):
+        # Print out the raw values of the board
         return self.values
 
     def update_values(self, x, y, value):
-
+        # Update the value at a specific index
         index = self._convert(x, y)
 
         self.values[int(index)] = value
 
     def get_value(self, x, y):
-        
+        # Get the value at a specific index
         return self.values[self._convert(x, y)]
 
     def get_move(self, player):
+        # Get the user input for a move from one user
         index = input("Player {player}, please put your move in, comma seperated, no spaces!")
     
         index_list = list(index) 
@@ -46,6 +58,7 @@ class Board:
         return index_list
 
     def validate_move(self, x, y, player):
+        # Check if move is a valid move
         _index = self._convert(x, y)
 
         if self.values[_index] == None:
@@ -67,7 +80,8 @@ class Board:
 
 
     def get_board_state(self, board):
-       
+        # Check of someone has won      
+ 
          for i in range(0, int(self.dim)):
             x_row = 0
             o_row = 0
@@ -106,4 +120,5 @@ def update_column(board):
     board.update_values(1, 1, "X")
     board.update_values(2, 1, "X")
     board.update_values(3, 1, "X")
+
 
