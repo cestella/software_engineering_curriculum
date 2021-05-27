2d40e0a1fbe63cca37b15b22cbb122b17ee6e85f
wastella <william.a.stella@gmail.com>
2021-05-27 16:12:37 -0400

    done

--- tic_tac_toe/src/board.py
+++ tic_tac_toe/src/board.py
@@ -19,7 +19,7 @@ class Board:
 
         self.winner = None
 
-    def _convert(self, x, y):
+    def convert(self, x, y):
         """
         converts a 2-dimensional coordinate to a 1-dimensional coordinate
         Parameters
@@ -72,24 +72,24 @@ class Board:
         : boolean
         Whether or not it is a valid move
         """
-        _index = self._convert(x, y)
+        _index = self.convert(x, y)
 
         if self.values[_index] == None:
             return True
 
         elif self.values[_index] == "X":
             if player == "X":
-                return True
+                return False
 
             elif player == "O":
-                return False
+                return True
 
         elif self.values[_index] == "O":
             if player == "O":
-                return True
+                return False
 
             elif player == "X":
-                return False
+                return True
 
     def check_column(self, board, x_or_o):
         """
@@ -113,7 +113,7 @@ class Board:
         for i in range(0, board_width):
             values = set()
             for j in range(0, board_width):
-                index = self._convert(i, j)
+                index = self.convert(i, j)
                 if board.values[index] != None:
                     values.add(board.values[index])
             if len(values) == 1:
@@ -146,7 +146,7 @@ class Board:
         for i in range(0, board_width):
             values = set()
             for j in range(0, board_width):
-                index = self._convert(i, j) + 1
+                index = self.convert(i, j) + 1
                 if board.values[index] != None:
                     values.add(board.values[index])
             if len(values) == 1:
@@ -200,41 +200,4 @@ class Board:
             return winner
 
 
-def update_column(board):
-    board.update_values(3, 1, "X")
-    board.update_values(3, 2, "X")
-    board.update_values(3, 3, "X")
 
-
-def update_diagonal(board):
-    board.update_values(3, 3, "X")
-    board.update_values(2, 2, "X")
-    board.update_values(1, 1, "X")
-
-
-def update_row(board):
-    board.update_values(1, 3, "X")
-    board.update_values(2, 3, "X")
-    board.update_values(3, 3, "X")
-
-
-def test_column(board):
-    update_column(board)
-    assert board.check_column(board, "X") == True
-
-
-def test_row(board):
-    update_row(board)
-    assert board.check_row(board, "X") == True
-
-
-def test_diagonal(board):
-    update_diagonal(board)
-    assert board.check_diagonal(board, "X") == True
-
-
-board = Board(3)
-
-test_column(board)
-test_row(board)
-test_diagonal(board)
--- tic_tac_toe/src/board_test.py
+++ tic_tac_toe/src/board_test.py
@@ -2,22 +2,22 @@ from board import Board
 
 
 def update_column(board):
-    board.update_values(3, 1, "X")
-    board.update_values(3, 2, "X")
-    board.update_values(3, 3, "X")
-
+    board.values[board.convert(3, 1)] = "X"
+    board.values[board.convert(3, 2)] = "X"
+    board.values[board.convert(3, 3)] = "X"
 
 def update_diagonal(board):
-    board.update_values(3, 3, "X")
-    board.update_values(2, 2, "X")
-    board.update_values(1, 1, "X")
-
+    board.values[board.convert(3, 3)] = "X"
+    board.values[board.convert(2, 2)] = "X"
+    board.values[board.convert(1, 1)] = "X"
 
 def update_row(board):
-    board.update_values(1, 3, "X")
-    board.update_values(2, 3, "X")
-    board.update_values(3, 3, "X")
+    board.values[board.convert(1, 3)] = "X"
+    board.values[board.convert(2, 3)] = "X"
+    board.values[board.convert(3, 3)] = "X"
 
+def validate_move_update(board):
+    board.values[0] = "X"
 
 def test_column(board):
     update_column(board)
@@ -33,9 +33,13 @@ def test_diagonal(board):
     update_diagonal(board)
     assert board.check_diagonal(board, "X") == True
 
+def test_validate_move(board):
+    validate_move_update(board)
+    assert board.validate_move(1, 1, "X") == False
 
 board = Board(3)
 
 test_column(board)
 test_row(board)
 test_diagonal(board)
+test_validate_move(board)
